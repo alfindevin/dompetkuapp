bb23ace7e1522a779e5406a27b9532b4eb46007d
alfindevin <alfinalfinalfin416@gmail.com>
2026-09-02 15:50:58 +0700

    Update supabaseClient.ts

--- app/supabaseClient.ts
+++ app/supabaseClient.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://vndekdvxrqqzoqifccrb.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_fX0-I0xey_ZOxrhZT0gb8A_3S0FLuEB'; // masukkan anon key Anda yang asli di sini atau biarkan pakai process.env
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey)
