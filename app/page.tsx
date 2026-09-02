bdef041d5d953d338f9d0fbc56c3f4e04d42ddb9
alfindevin <alfinalfinalfin416@gmail.com>
2026-09-02 15:34:35 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from './supabaseClient';
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
