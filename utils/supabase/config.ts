2e3c4a074fe9a25eb3cf2e5b57becf2757a290ac
alfindevin <alfinalfinalfin416@gmail.com>
2026-09-07 12:08:50 +0700

    Create config.ts

---
+++ utils/supabase/config.ts
@@ -0,0 +1,4 @@
+export const SUPABASE_CONFIG = {
+  url: process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://vndekdvxrqqzoqifccrb.supabase.co',
+  anonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbG...9K0Y',
+};
